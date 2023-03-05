4a36d8596fa6ce61d9d3d78d5f94f44383fd7c08
CaioLyra71 <caiolyra1@hotmail.com>
2023-03-05 10:45:39 -0300

    Exercicio 08 finalizado

---
+++ Exercício 08.js
@@ -0,0 +1,13 @@
+let numeros = [3, 4, 1, 8, 11, 7, 5];
+let comparacao = 0
+
+for (let numero of numeros) {
+
+    if (numero > comparacao) {
+        comparacao = numero
+    }
+}
+console.log(comparacao)
+
+
+
