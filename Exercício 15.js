141d77c26c1eccc1993662dee6a0b3429ad5bfd7
CaioLyra71 <caiolyra1@hotmail.com>
2023-03-06 17:05:52 -0300

    Exercício 15 finalizado utilizando os 3 loops

--- Exercício 15.js
+++ Exercício 15.js
@@ -2,14 +2,43 @@ const original = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 i = 0
 let pares = []
 let impares = []
-while (i < original.length) {
-    if (original[i] % 2 === 0) {
-        pares.push(original[i])
+
+// Utilizando while
+
+// while (i < original.length) {
+//     if (original[i] % 2 === 0) {
+//         pares.push(original[i])
+
+//     } else {
+//         impares.push(original[i])
+//     }
+//     i++
+// }
+// console.log(`Os numeros pares são ${pares}`)
+// console.log(`Os numeros ímpares são ${impares}`)
+
+// Utilizando for tradicional
+
+// for (i = 0; i < original.length; i++) {
+//     if (original[i] % 2 === 0) {
+//         pares.push(original[i])
+
+//     } else {
+//         impares.push(original[i])
+//     }
+// }
+// console.log(`Os numeros pares são ${pares}`)
+// console.log(`Os numeros ímpares são ${impares}`)
+
+// Utilizando For of
+
+for (numero of original) {
+    if (numero % 2 === 0) {
+        pares.push(numero)
 
     } else {
-        impares.push(original[i])
+        impares.push(numero)
     }
-    i++
 }
 console.log(`Os numeros pares são ${pares}`)
 console.log(`Os numeros ímpares são ${impares}`)
