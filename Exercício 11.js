f0aa1432c9c5dd42d1d72e21ee3caa6908efa74b
CaioLyra71 <caiolyra1@hotmail.com>
2023-03-06 09:53:54 -0300

    Exercício 11 finalizado

---
+++ Exercício 11.js
@@ -0,0 +1,11 @@
+const original = [5, 7, 10, 13, 17, 21, 26, 34, 100, 118, 245];
+let novoArray = []
+
+for (numero of original) {
+    if (numero >= 10 && numero <= 20 || numero > 100) {
+        novoArray.push(numero)
+
+    }
+
+}
+console.log(novoArray)
